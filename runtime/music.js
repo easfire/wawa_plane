5cfb6cce3e67e0f66c61e1bea0251f494bbb43e1
张翼 <zhangyi@sh-zhangyi01.local>
2018-08-12 18:03:14 +0800

    bg music

--- runtime/music.js
+++ runtime/music.js
@@ -11,7 +11,7 @@ export default class Music
 
     this.bgmAudio = new Audio();
     this.bgmAudio.loop = true;
-    this.bgmAudio.src = 'audio/summer.mp3';
+    this.bgmAudio.src = 'audio/xhlf.mp3';
     // this.bgmAudio.src = "https://m10.music.126.net/20180809180235/3cf13d3601481b01ed30443c6be32740/ymusic/20b6/9353/1b14/1dc83412677b7b8e0e01e04ef7935722.mp3";
 
     this.shootAudio     = new Audio();
